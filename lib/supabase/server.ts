19f3046323ba6138129ae124671c7a132e2fb552
عبدالله رافع ابوصغيرة <abdullahabusaghierh@my.uopeople.edu>
2026-03-09 00:39:51 +0300

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,41 +1,41 @@
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
-export async function createClient() {
+export function createClient() {
 const cookieStore = cookies()
 return createServerClient(
-process.env.NEXT_PUBLIC_SUPABASE_URL!,
-process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
 {
 cookies: {
 getAll() {
 return cookieStore.getAll()
 },
-setAll(cookiesToSet: { name: string; value: string; options: any }[]) {
+setAll(cookiesToSet: { name: string; value: string; options?: any }[]) {
 try {
-cookiesToSet.forEach(({ name, value, options }) =>
+cookiesToSet.forEach(({ name, value, options }) => {
 cookieStore.set(name, value, options)
-)
+})
 } catch {}
 },
 },
 }
 )
 }
-export async function createAdminClient() {
+export function createAdminClient() {
 const cookieStore = cookies()
 return createServerClient(
-process.env.NEXT_PUBLIC_SUPABASE_URL!,
-process.env.SUPABASE_SERVICE_ROLE_KEY!,
+process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+process.env.SUPABASE_SERVICE_ROLE_KEY as string,
 {
 cookies: {
 getAll() {
 return cookieStore.getAll()
 },
-setAll(cookiesToSet: { name: string; value: string; options: any }[]) {
+setAll(cookiesToSet: { name: string; value: string; options?: any }[]) {
 try {
-cookiesToSet.forEach(({ name, value, options }) =>
+cookiesToSet.forEach(({ name, value, options }) => {
 cookieStore.set(name, value, options)
-)
+})
 } catch {}
 },
 },
