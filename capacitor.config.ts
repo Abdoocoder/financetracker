905bc3c5b476f8bbde83c1a27efb3c67115afef0
abdoocoder <AbdullahAbuSaghierh@my.uopeople.edu>
2026-03-17 06:34:23 +0300

    fix: persist session in Android WebView

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,6 +7,8 @@ const config: CapacitorConfig = {
   server: {
     url: 'https://fajrak.com',
     cleartext: false,
+    androidScheme: 'https',
+    hostname: 'fajrak.com',
   },
   plugins: {
     PushNotifications: {
