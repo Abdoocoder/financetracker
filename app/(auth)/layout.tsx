d9f96d17edfa16c31e2fae94703673f220af07e0
abdoocoder <AbdullahAbuSaghierh@my.uopeople.edu>
2026-03-18 02:12:12 +0300

    fix: add I18nProvider to auth layout for reset password

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+import { I18nProvider } from '@/lib/i18n'
+
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <I18nProvider>
+      {children}
+    </I18nProvider>
+  )
+}
