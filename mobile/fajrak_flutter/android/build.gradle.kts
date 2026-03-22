892a72fd1475ad84f6b76307c536356a7372e1a0
Abdullah Abu Saghierh <en.abdooraf3@gmail.com>
2026-03-22 22:30:12 +0300

    feat: Initialize Android build configuration with centralized output directory and dynamic namespace generation.

--- mobile/fajrak_flutter/android/build.gradle.kts
+++ mobile/fajrak_flutter/android/build.gradle.kts
@@ -12,9 +12,27 @@ val newBuildDir: Directory =
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    val subproject = this
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(subproject.name)
+    subproject.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
+subprojects {
+    val subproject = this
+    if (subproject.name != "app") {
+        subproject.afterEvaluate {
+            if (subproject.plugins.hasPlugin("com.android.library") || subproject.plugins.hasPlugin("com.android.application")) {
+                val android = subproject.extensions.findByName("android") as? com.android.build.gradle.BaseExtension
+                android?.apply {
+                    if (namespace == null) {
+                        namespace = "com.fajrak.app.plugins.${subproject.name.replace("-", "_")}"
+                    }
+                }
+            }
+        }
+    }
+}
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
