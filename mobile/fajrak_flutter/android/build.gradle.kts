cfa56c0d228ae9ee1c169d50abaf04c22249cec7
Abdoocoder <en.abdooraf3@gmail.com>
2026-04-16 00:42:47 +0300

    feat: initialize Android build configuration using Kotlin DSL

--- mobile/fajrak_flutter/android/build.gradle.kts
+++ mobile/fajrak_flutter/android/build.gradle.kts
@@ -11,6 +11,10 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+    afterEvaluate {
+        extensions.findByType(com.android.build.gradle.BaseExtension::class.java)
+            ?.compileSdkVersion(36)
+    }
 }
 
 subprojects {
